87839964a605a1338d2bbfecd695c6c9e2e60c4d
Fedor Parfenov <fvpDev@outlook.com>
2016-02-03 12:05:22 -0500

    init

--- client/main/main.js
+++ client/main/main.js
@@ -1,8 +1,14 @@
 Template.main.viewmodel({
 	message: 'asdf',
+	onCreated: function () {
+		console.log('onCreated:', this.message());
+	},
+	onRendered: function () {
+		console.log('onRendered:', this.message());
+	},
 	autorun: [
 		function () {
-			console.log('msg:', message);
+			console.log('autorun:', this.message());
 		}
 	]
 });
