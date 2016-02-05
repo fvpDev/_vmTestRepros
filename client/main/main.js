089402323f6c79817368753e817a09437acb3138
Fedor Parfenov <fvpDev@outlook.com>
2016-02-05 15:27:44 -0500

    When mixin property is function, it is not overwritten...
    When mixin property is function, it does not get overwritten neither
    with direct property nor context input.

--- client/main/main.js
+++ client/main/main.js
@@ -1,14 +1,30 @@
-Template.main.viewmodel({
-	message: 'asdf',
-	onCreated: function () {
-		console.log('onCreated:', this.message());
+ViewModel.mixin({
+	main1Mixin: {
+		message: function () {
+			return 'main1_mixin';
+		}
 	},
-	onRendered: function () {
-		console.log('onRendered:', this.message());
-	},
+	main2Mixin: {
+		message: 'main2_mixin'
+	}
+});
+
+Template.main1.viewmodel({
+	mixin: 'main1Mixin',
+	message: 'main1_directProp',
 	autorun: [
 		function () {
-			console.log('autorun:', this.message());
+			console.log('main 1 autorun:', this.message());
+		}
+	]
+});
+
+Template.main2.viewmodel({
+	mixin: 'main2Mixin',
+	message: 'main2_directProp',
+	autorun: [
+		function () {
+			console.log('main 2 autorun:', this.message());
 		}
 	]
 });
